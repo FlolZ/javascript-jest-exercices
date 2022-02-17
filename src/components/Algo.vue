3b00ead74c8874da8dfbafc2e5b05abb6b968c52
Florian <f.horrut@dolist.com>
2022-02-17 12:31:43 +0100

    refacto

--- src/components/Algo.vue
+++ src/components/Algo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="padding: 40px;">
+  <div style="padding: 40px">
     <div
       justify="center"
       align="center"
@@ -12,126 +12,29 @@
       Javascript funny moments
     </div>
     <v-card style="padding: 80px">
-      <v-col class="mb-15">
+      <v-col class="mb-15" v-for="(item, i) in items" :key="item.title">
         <v-row>
           <v-avatar color="indigo" size="36">
-            <span class="white--text text-h5">1</span>
+            <span class="white--text text-h5">{{i+1}}</span>
           </v-avatar>
-          <h2>Exercice 1</h2>
+          <h2 class="ml-3">{{ item.title }}</h2>
         </v-row>
         <v-row>
           <h3>
-            Écrire une fonction qui retourne l’alphabet en minuscule, dans
-            l’ordre croissant, à partir de la lettre ’a’.
+            {{ item.statement }}
           </h3>
         </v-row>
-        <v-row> Résultat: </v-row>
-      </v-col>
-
-      <v-col class="mb-15">
-        <v-row>
-          <v-avatar color="indigo" size="36">
-            <span class="white--text text-h5">2</span>
-          </v-avatar>
-          <h2>Exercice 2</h2>
+        <v-row v-if="item.example">
+          <p>{{ item.example }}</p>
         </v-row>
-        <v-row>
-          <h3>
-            Écrire une fonction qui retourne l’alphabet en minuscule sur une
-            seule ligne, dans l’ordre décroissant, à partir de la lettre ’z’.
-          </h3>
-        </v-row>
-        <v-row> Résultat: </v-row>
-      </v-col>
-
-      <v-col class="mb-15">
-        <v-row>
-          <v-avatar color="indigo" size="36">
-            <span class="white--text text-h5">3</span>
-          </v-avatar>
-          <h2>Exercice 2</h2>
-        </v-row>
-        <v-row>
-          <h3>
-            Écrire une fonction qui retourne tous les chiffres, dans l’ordre
-            croissant.
-          </h3>
-        </v-row>
-        <v-row> Résultat: </v-row>
-      </v-col>
-
-      <v-col class="mb-15">
-        <v-row>
-          <v-avatar color="indigo" size="36">
-            <span class="white--text text-h5">4</span>
-          </v-avatar>
-          <h2>Exercice 4</h2>
-        </v-row>
-        <v-row>
-          <h3>
-            Écrire une fonction qui retourne ’N’ ou ’P’ suivant le signe de
-            l’entier passé en paramètre. Si n est négatif alors retourner ’N’.
-            Si n est positif alors retourner ’P’.
-          </h3>
-        </v-row>
-        <v-row> Résultat: </v-row>
-      </v-col>
-
-      <v-col class="mb-15">
-        <v-row>
-          <v-avatar color="indigo" size="36">
-            <span class="white--text text-h5">5</span>
-          </v-avatar>
-          <h2>Exercice 5</h2>
-        </v-row>
-        <v-row>
-          <h3>
-            Écrire une fonction qui retourne, dans l’ordre croissant, toutes les
-            différentes combinaisons de trois chiffres différents dans l’ordre
-            croissant - oui, la répétition est volontaire.
-          </h3>
-        </v-row>
-        <v-row>
-          <p>
-            Cela donne quelque chose comme ça :
-            <br />
-            012, 013, 014, 015, 016, 017, 018, 019, 023, ..., 789
-            <br />
-            987 n’est pas là car 789 est déjà présent 999 n’est pas là car ce
-            nombre ne comporte pas exclusivement des chiffres différents
-          </p>
-        </v-row>
-        <v-row> Résultat: </v-row>
-      </v-col>
-
-      <v-col class="mb-15">
-        <v-row>
-          <v-avatar color="indigo" size="36">
-            <span class="white--text text-h5">6</span>
-          </v-avatar>
-          <h2>Exercice 6</h2>
-        </v-row>
-        <v-row>
-          <h3>
-            Écrire une fonction qui retourne toutes les différentes combinaisons
-            de deux nombres entre 0 et 99, dans l’ordre croissant.
-          </h3>
-        </v-row>
-        <v-row>
-          <p>
-            Cela donne quelque chose comme ça :
-            <br />
-            00 01, 00 02, 00 03, 00 04, 00 05, ..., 00 99, 01 02, ..., 97 99, 98
-            99
-          </p>
-        </v-row>
-        <v-row> Résultat: </v-row>
+        <v-row> Résultat:</v-row>
       </v-col>
     </v-card>
   </div>
 </template>
 
 <script lang="ts">
+import { EXERCICES } from "./EXERCICES";
 import {
   computed,
   defineComponent,
@@ -142,9 +45,18 @@ import {
 export default defineComponent({
   props: {},
   setup(props) {
-    const data = reactive({});
+    const data = reactive({
+      items: EXERCICES
+    });
+
+    const exerciceResult = computed(() => {
+      return 'toto';
+    });
+
+
     return {
       ...toRefs(data),
+      exerciceResult
     };
   },
 });
---
+++ src/components/EXERCICES.ts
@@ -0,0 +1,49 @@
+export const EXERCICES = [
+  {
+    title: "Exercice 1",
+    statement: `Écrire une fonction qui retourne l’alphabet en minuscule, dans
+        l’ordre croissant, à partir de la lettre ’a’.`,
+    example: null,
+  },
+
+  {
+    title: "Exercice 2",
+    statement: `Écrire une fonction qui retourne l’alphabet en minuscule sur une
+    seule ligne, dans l’ordre décroissant, à partir de la lettre ’z’`,
+    example: null,
+  },
+
+  {
+    title: "Exercice 3",
+    statement: `Écrire une fonction qui retourne tous les chiffres, dans l’ordre
+    croissant.`,
+    example: null,
+  },
+
+  {
+    title: "Exercice 4",
+    statement: `Écrire une fonction qui retourne ’N’ ou ’P’ suivant le signe de
+    l’entier passé en paramètre. Si n est négatif alors retourner ’N’.
+    Si n est positif alors retourner ’P’`,
+    example: null,
+  },
+
+  {
+    title: "Exercice 5",
+    statement: `Écrire une fonction qui retourne, dans l’ordre croissant, toutes les
+    différentes combinaisons de trois chiffres différents dans l’ordre
+    croissant - oui, la répétition est volontaire.`,
+    example: `012, 013, 014, 015, 016, 017, 018, 019, 023, ..., 789
+        987 n’est pas là car 789 est déjà présent 999 n’est pas là car ce
+        nombre ne comporte pas exclusivement des chiffres différents
+    `,
+  },
+
+  {
+    title: "Exercice 6",
+    statement: `Écrire une fonction qui retourne toutes les différentes combinaisons
+    de deux nombres entre 0 et 99, dans l’ordre croissant.`,
+    example:
+      "00 01, 00 02, 00 03, 00 04, 00 05, ..., 00 99, 01 02, ..., 97 99, 98, 99",
+  },
+];
